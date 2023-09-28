b8a8a4693ab828da5a405e5b2eb0f6d4dd1d07c9
marioa25 <marioa25@ucm.es>
2023-09-28 14:49:47 +0200

    Error de referencia, esperando respuesta profesor

--- practica2/README.md
+++ practica2/README.md
@@ -1,2 +1,9 @@
-# Entregable 1 - station
+# MODO COMBINADO ESTACION / PUNTO DE ACCESO
 
+SALIDA DE LA CONEXIÓN COMO ESTACION:
+```
+I (5550) esp_netif_handlers: sta ip: 192.168.43.198, mask: 255.255.255.0, gw: 192.168.43.1
+I (5550) wifi station: got ip:192.168.43.198
+I (5550) wifi station: connected to ap SSID:RPI1_test password:test1234
+I (5560) main_task: Returned from app_main()
+```
--- practica2/entregable3/main/station_example_main.c
+++ practica2/entregable3/main/station_example_main.c
@@ -15,9 +15,14 @@
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
+#include "esp_mac.h"
+#include "esp_wifi_default.h"
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
+
+
+
 
 /* The examples use WiFi configuration that you can set via project configuration menu
 
@@ -27,6 +32,12 @@
 #define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
 #define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
 #define EXAMPLE_ESP_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
+
+
+#define EXAMPLE_ESP_WIFI_AP_SSID      CONFIG_ESP_WIFI_AP_SSID
+#define EXAMPLE_ESP_WIFI_AP_PASS      CONFIG_ESP_WIFI_AP_PASSWORD
+#define EXAMPLE_ESP_WIFI_AP_CHANNEL   CONFIG_ESP_WIFI_AP_CHANNEL
+#define EXAMPLE_ESP_WIFI_AP_MAX_CONN  CONFIG_ESP_WIFI_AP_MAX_CONN
 
 #if CONFIG_ESP_WPA3_SAE_PWE_HUNT_AND_PECK
 #define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HUNT_AND_PECK
@@ -38,6 +49,7 @@
 #define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_BOTH
 #define EXAMPLE_H2E_IDENTIFIER CONFIG_ESP_WIFI_PW_ID
 #endif
+
 #if CONFIG_ESP_WIFI_AUTH_OPEN
 #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_OPEN
 #elif CONFIG_ESP_WIFI_AUTH_WEP
@@ -70,12 +82,17 @@ static const char *TAG = "wifi station";
 static int s_retry_num = 0;
 
 
-static void event_handler(void* arg, esp_event_base_t event_base,
-                                int32_t event_id, void* event_data)
+
+/***************************************************
+* FUNCIONES PARA EL TRATAMIENDO DE EVENTOS - INICIO
+****************************************************/
+
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
         esp_wifi_connect();
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+
         if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
             esp_wifi_connect();
             s_retry_num++;
@@ -84,66 +101,155 @@ static void event_handler(void* arg, esp_event_base_t event_base,
             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
         }
         ESP_LOGI(TAG,"connect to the AP fail");
+
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
         ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
         ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
         s_retry_num = 0;
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
+
 }
 
-void wifi_init_sta(void)
+
+static void wifi_event_handler_start(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+    ESP_LOGI(TAG, "Driver WIFI iniciado correctamente.");
+}
+
+static void wifi_event_handler_stop(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+    ESP_LOGI(TAG, "Detectada parada del driver WIFI. Driver WIFI cerrado con exito.");
+}
+
+static void wifi_event_handler_staconnected(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+    wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+    ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",MAC2STR(event->mac), event->aid);
+}
+
+static void wifi_event_handler_stadisconnected(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+    wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+    ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",MAC2STR(event->mac), event->aid);
+}
+
+
+/***************************************************
+* FUNCIONES PARA EL TRATAMIENDO DE EVENTOS - FIN
+****************************************************/
+
+void wifi_init_apsta(void)
 {
     s_wifi_event_group = xEventGroupCreate();
 
+
+    //*** INICIO DE LA CONFIGURACIÓN ***
+    //*
+
+    //PASO 1..: Creamos e inicializamos los elementos necesarios para operar el WIFI.
     ESP_ERROR_CHECK(esp_netif_init());
-
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
-
+    esp_netif_create_default_wifi_sta();        //Creamos la interfaz con la pila TCP/IP para el modo Estacion
+    esp_netif_create_default_wifi_ap();         //Creamos la interfaz con la pila TCP/IP para el modo Punto de Acceso
+    
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
     esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
                                                         ESP_EVENT_ANY_ID,
                                                         &event_handler,
                                                         NULL,
                                                         &instance_any_id));
+
+    esp_event_handler_instance_t instance_got_ip;
     ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
                                                         IP_EVENT_STA_GOT_IP,
                                                         &event_handler,
                                                         NULL,
                                                         &instance_got_ip));
 
-    wifi_config_t wifi_config = {
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        WIFI_EVENT_AP_START,
+                                                        &wifi_event_handler_start,
+                                                        NULL,
+                                                        NULL));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        WIFI_EVENT_AP_STOP,
+                                                        &wifi_event_handler_stop,
+                                                        NULL,
+                                                        NULL));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        WIFI_EVENT_AP_STACONNECTED,
+                                                        &wifi_event_handler_staconnected,
+                                                        NULL,
+                                                        NULL));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        WIFI_EVENT_AP_STADISCONNECTED,
+                                                        &wifi_event_handler_stadisconnected,
+                                                        NULL,
+                                                        NULL));
+
+
+
+    //PASO 2..: Configuramos la conexión a realizar y especificamos los parámetros 
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA) );       //Indicamos el modo de conexión combinado
+    
+    wifi_config_t wifiConfig_STAmode = {
         .sta = {
             .ssid = EXAMPLE_ESP_WIFI_SSID,
             .password = EXAMPLE_ESP_WIFI_PASS,
-            /* Authmode threshold resets to WPA2 as default if password matches WPA2 standards (pasword len => 8).
-             * If you want to connect the device to deprecated WEP/WPA networks, Please set the threshold value
-             * to WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK and set the password with length and format matching to
-             * WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK standards.
-             */
             .threshold.authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD,
             .sae_pwe_h2e = ESP_WIFI_SAE_MODE,
             .sae_h2e_identifier = EXAMPLE_H2E_IDENTIFIER,
         },
     };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_start() );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifiConfig_STAmode) );     //Configuramos el modo Punto de Acceso
 
+
+    wifi_config_t wifiConfig_APMode = {
+        .ap = {
+            .ssid = EXAMPLE_ESP_WIFI_AP_SSID,
+            .ssid_len = strlen(EXAMPLE_ESP_WIFI_AP_SSID),
+            .channel = EXAMPLE_ESP_WIFI_AP_CHANNEL,
+            .password = EXAMPLE_ESP_WIFI_AP_PASS,
+            .max_connection = EXAMPLE_ESP_WIFI_AP_MAX_CONN,
+            #ifdef CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT
+                .authmode = WIFI_AUTH_WPA3_PSK,
+                .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+            #else /* CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT */
+                .authmode = WIFI_AUTH_WPA2_PSK,
+            #endif
+            .pmf_cfg = {
+                    .required = true,
+            },
+        },
+    };
+    if (strlen(EXAMPLE_ESP_WIFI_AP_PASS) == 0) {
+        wifiConfig_APMode.ap.authmode = WIFI_AUTH_OPEN;
+    }
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifiConfig_APMode) );    //Configuramos el modo Estación
+
+
+    //PASO 3..: Iniciamos la cofiguración del driver WIFI
+    ESP_ERROR_CHECK(esp_wifi_start());
     ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    //*
+    //*** FIN DE LA CONFIGURACIÓN ***
+
 
     /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
      * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
     EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
+                                            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                            pdFALSE,
+                                            pdFALSE,
+                                            portMAX_DELAY);
 
     /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
      * happened. */
@@ -158,6 +264,7 @@ void wifi_init_sta(void)
     }
 }
 
+
 void app_main(void)
 {
     //Initialize NVS
@@ -168,6 +275,6 @@ void app_main(void)
     }
     ESP_ERROR_CHECK(ret);
 
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
-    wifi_init_sta();
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_APSTA");
+    wifi_init_apsta();
 }
